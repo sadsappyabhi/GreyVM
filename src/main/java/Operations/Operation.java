70d0f213170bbaa0353f6dc2a6d130db587db143
Abhilasha <sadsappyabhi@proton.me>
2024-10-16 23:00:30 -0400

    Moved Operation Interface to Operations package

---
+++ src/main/java/Operations/Operation.java
@@ -0,0 +1,20 @@
+import Operations.SymbolTable;
+
+import java.util.Stack;
+/**
+ * An object that implements this interface will represent a single statement
+ * of the program
+ *
+ * @Author Abhilasha Chebolu
+ * @Version Summer/Fall 2024
+ */
+public interface Operation {
+    /**
+     * Executes a single operation
+     *
+     * @param programCounter Index of the Operation
+     * @param stack The Execution Stack
+     * @param symbolTable The Symbol Table
+     */
+    int execute(int programCounter, Stack<Integer> stack, SymbolTable symbolTable);
+}
