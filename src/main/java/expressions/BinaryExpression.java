d274474aaec6208a1092884a99c0dce287aa0cb7
Chebolu <Abhilasha.Chebolu@charter.com>
2024-10-18 13:12:33 -0400

    added BinaryExpression

--- src/main/java/expressions/BinaryExpression.java
+++ src/main/java/expressions/BinaryExpression.java
@@ -1,4 +1,28 @@
 package expressions;
 
-public class BinaryExpression {
+import jdk.incubator.vector.VectorOperators;
+
+public class BinaryExpression extends AbstractExpression {
+    private AbstractExpression leftChild;
+    private AbstractExpression rightChild;
+    Operator op;
+
+    public BinaryExpression(AbstractExpression left, Operator op, AbstractExpression right) {
+        this.leftChild = left;
+        this.op = op;
+        this.rightChild = right;
+    }
+
+    public Operator getOperator() {
+        return op;
+    }
+
+    @Override
+    public void accept(Visitor v) {
+        v.preVisit(this);
+        leftChild.accept(v);
+        v.visit(this);
+        rightChild.accept(v);
+        v.postVisit(this);
+    }
 }
